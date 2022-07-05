3dd543c4be80a91f26423a11b6f73f41021cdd10
elyes.fakhar@gmail.com <elyes.fakhar@gmail.com>
2022-07-05 15:53:34 +0100

    saving to file

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,16 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+
 
 namespace TrackerLibrary.DataAccess
 {
-    // TODO -   Wire up the CreatePrize for text files.
-
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrivateModel.csv";
+
+        // TODO -   Wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModel();
+
+            int CurrentId = prizes.Count == 0 ? 1 : prizes.OrderByDescending(x => x.Id).First().Id + 1;
+
+            model.Id = CurrentId;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
