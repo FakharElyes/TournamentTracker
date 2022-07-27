fecdb19fa81703c8586c527066467f41410d6cc9
elyes.fakhar@gmail.com <elyes.fakhar@gmail.com>
2022-07-27 12:39:04 +0100

    Tournament Viewer Part 1

--- TrackerLibrary/Models/MatchupModel.cs
+++ TrackerLibrary/Models/MatchupModel.cs
@@ -8,6 +8,8 @@ namespace TrackerLibrary.Models
 {
     public class MatchupModel
     {
+        private readonly string notDetermined = "Matchup Not Yet Determined";
+
         public int Id { get; set; }
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
 
@@ -18,5 +20,36 @@ namespace TrackerLibrary.Models
 
         public TeamModel Winner { get; set; }
         public int MatchupRound { get; set; }
+
+
+        public string DisplayName
+        {
+            get
+            {
+                string output = string.Empty;
+
+                foreach (MatchupEntryModel me in Entries)
+                {
+                    if (me.TeamCompeting != null)
+                    {
+                        if (string.IsNullOrEmpty(output))
+                        {
+                            output = me.TeamCompeting.TeamName;
+                        }
+                        else
+                        {
+                            output += $" vs. {me.TeamCompeting.TeamName}";
+                        } 
+                    }
+                    else
+                    {
+                        output = notDetermined;
+                        break;
+                    }
+                }
+
+                return output;
+            }
+        }
     }
 }
--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -1,15 +1,111 @@
+using System.ComponentModel;
+using TrackerLibrary.Models;
+
 namespace TrackerUI
 {
     public partial class TournamentViewerForm : Form
     {
-        public TournamentViewerForm()
+        private readonly string teamNotSet = "Not Yet Set";
+        private TournamentModel tournamentModel;
+        BindingList<int> rounds = new();
+        BindingList<MatchupModel> selectedMatchups = new();
+
+        public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
+            this.tournamentModel = tournamentModel;
+
+            WireUpLists();
+
+            LoadFormData();
+            LoadRounds();
+        }
+
+        private void LoadMatchup(MatchupModel m)
+        {
+            if (m != null)
+            {
+                for (int i = 0; i < m.Entries.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        teamOneName.Text = m.Entries[0].TeamCompeting != null ? m.Entries[0].TeamCompeting.TeamName : teamNotSet;
+                        teamOneScoreValue.Text = m.Entries[0].TeamCompeting != null ? m.Entries[0].Score.ToString() : String.Empty;
+
+                        teamTwoName.Text = teamNotSet;
+                        teamTwoScoreValue.Text = String.Empty;
+                    }
+
+                    if (i == 1)
+                    {
+                        teamTwoName.Text = m.Entries[1].TeamCompeting != null ? m.Entries[1].TeamCompeting.TeamName : teamNotSet;
+                        teamTwoScoreValue.Text = m.Entries[1].TeamCompeting != null ? m.Entries[1].Score.ToString() : String.Empty;
+                    }
+
+                } 
+            }
         }
 
         private void matchupListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            LoadMatchup((MatchupModel)matchupListBox.SelectedItem);
+        }
 
+        private void LoadFormData()
+        {
+            tournamentName.Text = this.tournamentModel.TournamentName;
+        }
+
+        private void WireUpLists()
+        {
+            roundDropDown.DataSource = rounds;
+            matchupListBox.DataSource = selectedMatchups;
+            matchupListBox.DisplayMember = "DisplayName";
+        }
+
+
+        private void LoadRounds()
+        {
+            rounds.Clear();
+
+            rounds.Add(1);
+            int currRound = 1;
+
+            foreach (List<MatchupModel> matchup in tournamentModel.Rounds)
+            {
+                if (matchup.Count > 0 && matchup.First().MatchupRound > currRound)
+                {
+                    currRound = matchup.First().MatchupRound;
+                    rounds.Add(currRound);
+                }
+            }
+
+            LoadMatchups(1);
+        }
+
+        private void roundDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadMatchups((int)roundDropDown.SelectedItem);
+        }
+
+        private void LoadMatchups(int round)
+        {
+            foreach (List<MatchupModel> matchups in tournamentModel.Rounds)
+            {
+                if (matchups.Count > 0 && matchups.First().MatchupRound == round)
+                {
+                    selectedMatchups.Clear();
+                    foreach (MatchupModel m in matchups)
+                    {
+                        selectedMatchups.Add(m);
+                    }
+                }
+            }
+
+            MatchupModel matchupModel = selectedMatchups.Count > 0 ? selectedMatchups.First() : new MatchupModel() ;
+
+
+            LoadMatchup(matchupModel);
         }
     }
 }
